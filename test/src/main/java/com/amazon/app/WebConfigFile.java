3cd8cb1b0aac7dbda77566702e040dfbab1aed46
Markandeyulu Kothapu <markandeyulu.kothapu@gmail.com>
2021-06-10 16:25:30 -0400

    new code changes

--- test/src/main/java/com/amazon/app/WebConfigFile.java
+++ test/src/main/java/com/amazon/app/WebConfigFile.java
@@ -1,11 +1,16 @@
 package com.amazon.app;
 
+import java.util.Properties;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.handler.BeanNameUrlHandlerMapping;
+import org.springframework.web.servlet.handler.SimpleUrlHandlerMapping;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
+import com.amazon.app.controller.ProductController2;
 import com.amazon.app.dao.ProductDAO;
 
 @Configuration
@@ -25,5 +30,27 @@ public class WebConfigFile {
 		irvr.setPrefix("/WEB-INF/views/");
 		irvr.setSuffix(".jsp");
 		return irvr;
+	}
+	
+	
+	
+	//@Bean("/testbeannamemapping*")
+	@Bean
+	public ProductController2 ProductController2() {
+		return new ProductController2();
+	}
+	
+/*	@Bean
+	public BeanNameUrlHandlerMapping getBeanNameUrlHandlerMapping() {
+		return new BeanNameUrlHandlerMapping();
+	}*/
+	
+	@Bean
+	public SimpleUrlHandlerMapping getSimpleUrlHandlerMapping() {
+		SimpleUrlHandlerMapping shm = new SimpleUrlHandlerMapping();
+		Properties mappings = new Properties();
+		mappings.setProperty("/marktestsimpleURL", "productController2");
+		shm.setMappings(mappings);
+		return shm;
 	}
 }
--- test/src/main/java/com/amazon/app/controller/ProductController2.java
+++ test/src/main/java/com/amazon/app/controller/ProductController2.java
@@ -1,5 +1,8 @@
 package com.amazon.app.controller;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,13 +13,14 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.AbstractController;
 
 //Back Controller
-@RestController()
-@RequestMapping("/product") //If URI's are unique you dont need identifier. this is just example identifier
-public class ProductController2 {
+//@RestController()
+//@RequestMapping("/product") //If URI's are unique you dont need identifier. this is just example identifier
+public class ProductController2  extends AbstractController {
  
-	@GetMapping(value = "/getproduct2") // same way we have postmapping and rest. latest usage
+	/*@GetMapping(value = "/getproduct2") // same way we have postmapping and rest. latest usage
 	public String getMessage() {
 		return "Welcome Rest controller" ;
 	}
@@ -24,6 +28,17 @@ public class ProductController2 {
 	@GetMapping(value = "/getproduct3") // same way we have postmapping and rest. latest usage
 	public String getMessage2(@RequestParam String id, @RequestParam String id2) {
 		return "Welcome Rest controller : " + id + id2;
+	}*/
+	
+	//@ResponseBody
+	//@GetMapping("/getproduct1") // same way we have postmapping and rest. latest usage
+	@Override
+	 protected ModelAndView handleRequestInternal(HttpServletRequest request,
+	            HttpServletResponse response) throws Exception {
+		ModelAndView mv = new ModelAndView("Welcome");
+		mv.addObject("message1", "MESSAGE1");
+		mv.addObject("message2", "MESSAGE2");
+		return mv;
 	}
 	 
 }
